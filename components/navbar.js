6923b04e1de36c95fe609d6974f4f38cb2ee1422
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2024-06-17 20:29:43 -0400

    updated navbar

--- components/navbar.js
+++ components/navbar.js
@@ -7,6 +7,7 @@ export default function Navbar({ user }) {
             <ul className='list-none inline'>
                <Link href='/'><li className='hover:text-blue-200 inline px-2'>home</li></Link>
                <Link href='/leaderboard'><li className='hover:text-blue-200 inline px-2'>Leaderboard</li></Link>
+               <Link href='/packOpening'><li className='hover:text-blue-200 inline px-2'>Pack Tracker</li></Link>
                {!user.id ? (
                   <Link href='/login'><li className='hover:text-blue-200 inline px-2'>login</li></Link>
                ) : (
